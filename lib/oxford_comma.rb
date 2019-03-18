5fce66a981570257d229ee30e2c7ac1cb266e805
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-18 22:59:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
-  array.join(",")
+    array.join(",")
   elsif array.length == 2 
-  array.join(" and ")
+    array.join(" and ")
   else
-  
+    arra[]
   end
 end
